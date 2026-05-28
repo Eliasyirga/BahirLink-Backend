22e9004e1eabeeca1e317b7e744ca672769d0e8f
Your Name <yayehasres1221@gmail.com>
2026-05-29 00:19:44 +0300

    updated

--- routes/caseReportsRoutes.js
+++ routes/caseReportsRoutes.js
@@ -4,15 +4,14 @@ const caseReportsController = require("../controllers/caseReportsController");
 const validator = require("../utils/validator");
 const { phoneValidationSchema } = require("../utils/schema");
 
-router.post(
-  "/",
-  validator(phoneValidationSchema),
-  caseReportsController.createReport,
-);
+router.post("/", caseReportsController.createReport);
 
 router.get("/", caseReportsController.getAllReports);
+
 router.get("/case/:caseId", caseReportsController.getReportsByCase);
+
 router.get("/type/:caseTypeId", caseReportsController.getReportsByCaseType);
+
 router.get("/reporter/:reporterId", caseReportsController.getReportsByReporter);
 
 router.patch("/status/:id", caseReportsController.updateReportStatus);
